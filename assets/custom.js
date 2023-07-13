0d218dc396723e834c7904d43695e0918f6303a5
AshDev0 <college.ashwani@gmail.com>
2023-07-13 12:51:54 +0530

    New Commit

--- assets/custom.js
+++ assets/custom.js
@@ -105,3 +105,32 @@ $('.owl_slider').owlCarousel({
 })
 
 */
+
+
+$(document).ready(function(){
+  $('#sortby').click(function(){
+    $('#sortbyContent').slideToggle("fast");
+    $('#arrow-btn').toggleClass('active');
+  })
+});
+
+
+$(document).on('click','a#collection-url', function(e){
+  e.preventDefault();
+  var pageURL=$(this).attr('href');
+  var curr_url = window.location.href;
+  console.log(pageURL);
+   history.pushState(null, '', pageURL);
+    
+   $.ajax({    
+      type: "GET",
+      url: curr_url, 
+      data:{page:pageURL},            
+      dataType: "html",                  
+      success: function(data){ 
+        console.log("Hello World");
+       $('#MainContent').html(data);    
+              
+      }
+  });
+});
